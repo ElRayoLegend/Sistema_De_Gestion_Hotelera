0ecc723ab1414c56d16dcbd2a88b33c6a0f0c5fe
Adrian Morataya <stevencalavera2@gmail.com>
2024-09-03 20:29:30 -0600

    Build:se creo el proyecto gestion hotelera el Arca de Noe

---
+++ src/main/java/com/gestionelarca/system/GestionHoteleraArcaDeNoeApplication.java
@@ -0,0 +1,13 @@
+package com.gestionelarca.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHoteleraArcaDeNoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHoteleraArcaDeNoeApplication.class, args);
+	}
+
+}
