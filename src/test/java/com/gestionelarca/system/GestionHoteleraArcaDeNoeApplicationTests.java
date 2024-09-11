a93441832673f9772de1323a326c210dbd0086f1
Oliver <osales-2020584@kinal.edu.gt>
2024-09-11 09:33:47 -0600

    Add:se agrego la carpeta src

---
+++ src/test/java/com/gestionelarca/system/GestionHoteleraArcaDeNoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionelarca.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHoteleraArcaDeNoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
